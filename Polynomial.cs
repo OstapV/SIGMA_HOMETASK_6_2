32d0098e78ad1d39db90eb2c1daaa5085c2adce9
OstapV <92176833+OstapV@users.noreply.github.com>
2021-10-11 23:41:10 +0300

    Add files via upload

---
+++ Polynomial.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polinomial
+{
+    class Polynomial
+    {
+        private double[] coef;
+        private int m_Step;
+
+        public double[] Coef { get { return coef;} set { coef = value; } }
+        public int M_Step { get { return m_Step; } set { m_Step = value; } }
+        public double this[int index]
+        {
+            get 
+            {
+                return coef[index];            
+            }
+            set
+            {
+                if (value != 0 && m_Step >= 0)
+                {
+                    coef[index] = value;
+                }
+
+                if(value != 0 && m_Step <= 0)
+                {
+                    m_Step = 0;
+                    coef = new double[m_Step + 1];
+                    coef[index] = value;
+                }
+         
+                if(value == 0 && m_Step >= 0)
+                {
+                    double[] arr = new double[this.coef.Length - 1];
+                    int j = 0;
+                    for (int i = 0; i < this.coef.Length; i++, j++)
+                    {
+                        if (i == index)
+                        {
+                            j--;
+                            continue;
+                        }
+                        arr[j] = this.coef[i];
+                    }
+
+                    this.coef = arr;
+                }
+
+                if (value == 0 && m_Step <= 0) { return; }
+              
+
+            }
+        
+        }
+
+        public Polynomial() { }
+        public Polynomial(double [] arr, int step)
+        {
+            m_Step = step;
+            coef = arr;
+        }
+
+        public static Polynomial operator +(Polynomial A, Polynomial B)
+        {
+            Polynomial C = new Polynomial();
+
+            C.Coef = new double[A.M_Step + 1];
+            C.M_Step = A.M_Step;
+
+            for (int i = 0; i < A.Coef.Length; i++)
+            {
+                C.Coef[i] = A.Coef[i] + B.Coef[i];
+            }
+
+            return C;
+        }
+
+
+        public static Polynomial operator -(Polynomial A, Polynomial B)
+        {
+            Polynomial C = new Polynomial();
+
+            C.Coef = new double[A.M_Step + 1];
+            C.M_Step = A.M_Step;
+
+            for (int i = 0; i < A.Coef.Length; i++)
+            {
+                C.Coef[i] = A.Coef[i] - B.Coef[i];
+            }
+
+            return C;
+        }
+
+        public static Polynomial operator *(Polynomial A, Polynomial B)
+        {
+            Polynomial C = new Polynomial();
+
+            C.Coef = new double[A.M_Step + 1 + B.M_Step];
+            C.M_Step = A.M_Step + B.M_Step;
+
+            for (int i = 0; i <= A.m_Step; i++)
+            {
+                for (int j = 0; j <= B.m_Step; j++)
+                {
+                    C[i + j] += A[i] * B[j];
+                }
+            }
+
+            return C;
+        }
+
+        public void Show()
+        {
+            for (int i = 1; i <= this.m_Step; i++)
+            {
+                Console.WriteLine($" + {this[i]}x^{i}");
+            }
+            
+        }
+
+
+        public static implicit operator Polynomial(double []coeff)
+        {
+            return new Polynomial(coeff, 0);
+        }
+
+        public void Parse(string polynom)
+        {
+            string[] monom = polynom.Replace(" ", String.Empty).Split('+');
+
+            m_Step = monom.Length - 1;
+            coef = new double[m_Step + 1];
+
+            for (int i = 0; i <= m_Step; i++)
+            {
+                if (!double.TryParse(monom[i].Split('x')[0], out coef[i]))
+                    throw new ArgumentException("Bad coefficient");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Polinomial
+{
+    using System.Globalization;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polynomial A = new Polynomial();
+            Console.WriteLine("Enter the grade of first polynomial:");  
+            A.M_Step = Convert.ToInt32(Console.ReadLine());
+
+            A.Coef = new double[A.M_Step + 1];
+ 
+            Console.WriteLine("Enter the coefficients of first polynomial");
+            for (int i = 0; i < A.Coef.Length; i++)
+            {
+                A.Coef[i] = Double.Parse(Console.ReadLine());
+            }
+
+            Polynomial B = new Polynomial();
+            Console.WriteLine("Enter the grade of second polynomial:");
+            B.M_Step = Convert.ToInt32(Console.ReadLine());
+ 
+            B.Coef = new double[B.M_Step+1];
+ 
+            Console.WriteLine("Enter the coefficients of first polynomial:");
+            for (int i = 0; i < B.Coef.Length; i++)
+            {
+                B.Coef[i] = Double.Parse(Console.ReadLine());
+            }
+
+            Console.Write("A= ");
+            A.Show();           
+            Console.WriteLine("\n");
+            Console.Write("B= ");
+            B.Show();
+
+            A.Coef[0] = 10;      // using indexator
+            Console.Write("A= ");
+            A.Show();
+
+            Polynomial C = new Polynomial();
+            C = A + B;
+            Polynomial D = new Polynomial();
+            D = A - B;
+            Polynomial E = new Polynomial();
+            E = A * B;
+            Console.Write("A+B=\n");
+            C.Show();
+            Console.Write("A-B= ");
+            D.Show();
+            Console.Write("A*B= ");
+            E.Show();    
+            Console.ReadLine();
+
+           
+
+            Polynomial test = new Polynomial();
+            Console.WriteLine("Please, enter polynomial: ");
+            string info = Console.ReadLine();
+            test.Parse(info);
+
+        }
+    }
+}
